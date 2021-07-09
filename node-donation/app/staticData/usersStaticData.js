4d74ab0a31e5bd1306cc85cc32c88db94c0edbea
Sanket Patel <3sanket3@gmail.com>
2021-07-09 23:05:20 +0530

    update users name

--- node-donation/app/staticData/usersStaticData.js
+++ node-donation/app/staticData/usersStaticData.js
@@ -3,33 +3,33 @@ const { v4: uuidv4 } = require('uuid');
 const usersStaticData = [
     {
       "id": "9e80502c-45ac-4936-946e-9959a06148ce",
-      "firstName": "FakeOneFirstName",
-      "lastName": "FakeOneLastName",
-      "email": "fakeOne@exmaple.com"
+      "firstName": "Maria",
+      "lastName": "Hernandez",
+      "email": "maria@exmaple.com"
     },
     {
       "id": "60235235-861e-43b6-bafe-c4dce32d3c44",
-      "firstName": "FakeTwoFirstName",
-      "lastName": "FakeTwoLastName",
-      "email": "fakeTwo@exmaple.com"
+      "firstName": "Michael",
+      "lastName": "Smith",
+      "email": "michael@exmaple.com"
     },
     {
       "id": "90a2648f-4ac1-42b4-a4f6-2478e9360131",
-      "firstName": "FakeThreeFirstName",
-      "lastName": "FakeThreeLastName",
-      "email": "fakeThree@exmaple.com"
+      "firstName": "Robert",
+      "lastName": "Garcia",
+      "email": "robert@exmaple.com"
     },
     {
       "id": "5d3f79ab-9137-4bc5-93a1-88adc3983d9c",
-      "firstName": "FakeFourFirstName",
-      "lastName": "FakeFourLastName",
-      "email": "fakeFour@exmaple.com"
+      "firstName": "David",
+      "lastName": "Smith",
+      "email": "david@exmaple.com"
     },
     {
       "id": "1ddab4a0-578b-466d-ad86-23f80103960a",
-      "firstName": "FakeFiveFirstName",
-      "lastName": "FakeFiveLastName",
-      "email": "fakeFive@exmaple.com"
+      "firstName": "John",
+      "lastName": "Millard",
+      "email": "john@exmaple.com"
     }
   ];
 
